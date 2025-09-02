d06a0a56d31f007696e3a8453eaa37f91ba34f1f
piaoxuebingfeng <526656549@qq.com>
2025-09-02 10:49:52 +0800

    添加 wifi 使用示例

---
+++ m5tab5_wifi_sta/m5tab5_wifi_sta.ino
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <M5GFX.h>
+#include <M5Unified.h>
+#include <WiFi.h>
+
+#define SDIO2_CLK GPIO_NUM_12
+#define SDIO2_CMD GPIO_NUM_13
+#define SDIO2_D0  GPIO_NUM_11
+#define SDIO2_D1  GPIO_NUM_10
+#define SDIO2_D2  GPIO_NUM_9
+#define SDIO2_D3  GPIO_NUM_8
+#define SDIO2_RST GPIO_NUM_15
+
+const char *ssid     = "xiaomi14";
+const char *password = "1234567890";
+
+void setup()
+{
+    M5.begin();
+    Serial.begin(115200);
+
+    M5.Display.setFont(&fonts::FreeMonoBoldOblique24pt7b);
+
+    WiFi.setPins(SDIO2_CLK, SDIO2_CMD, SDIO2_D0, SDIO2_D1, SDIO2_D2, SDIO2_D3, SDIO2_RST);
+
+    // If you select the M5Tab5 board in Arduino IDE, you could use the default pins defined.
+    // WiFi.setPins(BOARD_SDIO_ESP_HOSTED_CLK, BOARD_SDIO_ESP_HOSTED_CMD, BOARD_SDIO_ESP_HOSTED_D0,
+    //              BOARD_SDIO_ESP_HOSTED_D1, BOARD_SDIO_ESP_HOSTED_D2, BOARD_SDIO_ESP_HOSTED_D3,
+    //              BOARD_SDIO_ESP_HOSTED_RESET);
+
+    // STA MODE
+    WiFi.mode(WIFI_STA);
+    M5.Display.println("WiFi mode set to STA");
+    WiFi.begin(ssid, password);
+    M5.Display.print("Connecting to ");
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        M5.Display.print(".");
+    }
+    M5.Display.println("");
+    M5.Display.print("Connected to ");
+    M5.Display.println(ssid);
+    M5.Display.print("IP address: ");
+    M5.Display.println(WiFi.localIP());
+
+    // AP MODE
+    // WiFi.mode(WIFI_MODE_AP);
+    // Serial.println("WiFi mode set to AP");
+    // WiFi.softAP(ssid, password);
+    // Serial.println("AP started");
+    // Serial.print("IP address: ");
+    // Serial.println(WiFi.softAPIP());
+
+}
+
+void loop()
+{
+} 
